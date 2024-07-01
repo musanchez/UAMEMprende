9d0d7ff91705be17edfcfd4a60256aacc92665b2
Gabriel Chang <gdchang@uamv.edu.ni>
2024-06-30 22:35:21 -0600

    fixes cards Emprendimientos y redirects login/register

--- resources/views/emprendimientos/index.blade.php
+++ resources/views/emprendimientos/index.blade.php
@@ -3,18 +3,18 @@
 @section('content')
 
 <div class="container">
-    <h2 class="mb-4">Listado de Emprendimientos</h2>
+    <h2 class="mb-4" style="text-align: center">Listado de Emprendimientos</h2>
     <div class="row row-cols-1 row-cols-md-3 g-4">
         @foreach ($emprendimientos as $emprendimiento)
         <div class="col">
-            <div class="card">
+            <div class="card h-100">
                 <img src="{{ $emprendimiento->imagen }}" class="card-img-top" alt="{{ $emprendimiento->nombre }}" style="width: 100%; height: 200px; object-fit: cover;">
-                <div class="card-body">
+                <div class="card-body d-flex flex-column">
                     <h5 class="card-title">{{ $emprendimiento->nombre }}</h5>
-                    <p class="card-text">{{ \Illuminate\Support\Str::limit($emprendimiento->descripcion, 100, $end='...') }}</p>
+                    <p class="card-text flex-grow-1">{{ \Illuminate\Support\Str::limit($emprendimiento->descripcion, 100, $end='...') }}</p>
                     <p class="card-text"><strong>Emprendedor:</strong> {{ $emprendimiento->emprendedor->nombre }}</p>
                     <p class="card-text"><strong>Teléfono:</strong> {{ $emprendimiento->emprendedor->celular }}</p>
-                    <a href="#" class="btn btn-primary" style="background-color: #439FA5; border-color: #439FA5;">Ver más</a>
+                    <a href="#" class="btn btn-primary mt-auto" style="background-color: #439FA5; border-color: #439FA5;">Ver más</a>
                 </div>
             </div>
         </div>
